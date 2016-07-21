524b236878afb74a9b7192f11b81ba3022727b1e
Michael Berber <mickberber@icloud.com>
2016-07-21 14:24:15 -0700

    add comments and a test

--- problems/anagramPalindrome.js
+++ problems/anagramPalindrome.js
@@ -3,24 +3,41 @@ An anagram is a word that can be rearragnged to make another word
 ex: 'cato' is an angram of 'taco'
 A palindrome is a word that is the same forward and backwards
 ex: 'tacocat', 'eevee'
+
+
+This algorithm relys on the fact that a palindrome cannot be created if more than one letter occurs an odd number of times
+
+We will count the letters
+and check for this
+
+If there are 0 || 1 instances of odd occuring letters
+return true because a palindrome can be created
+
+Otherwise we will return false
+
 */
 
 module.exports = anagramPalindrome;
 
 function anagramPalindrome(word) {
+  //keep count of all letters in word in an object
   var letterCounts = {};
+  //iterate over input word
   for(var i = 0; i < word.length; i++) {
+    //set key to itself if defined OR create new key in letterCounts object
     letterCounts[word[i]] = letterCounts[word[i]] || 0;
+    //increment count
     letterCounts[word[i]]++;
   }
-
+  //create variable to count letterCounts that are Odd
   var oddAmounts = 0;
+  //iterate over letterCounts
   for(var letter in letterCounts) {
+    //if count of specific letter isnt divisble by 2
     if(letterCounts[letter] % 2 !== 0) {
+      //increment oddAmounts and check if it has been incremented more than once
       oddAmounts++;
       if(oddAmounts > 1) {
-        console.log(letterCounts)
-        console.log(oddAmounts)
         return false;
       }
     }
--- test/anagramPalindrome.js
+++ test/anagramPalindrome.js
@@ -4,5 +4,6 @@ var write = tests.write;
 var anagramPalindrome = require('../problems/anagramPalindrome.js');
 
 module.exports = write.testFile(
-  write.description('anagramPalindrome should return true when passed a string that can be a re-aranged to a palindrome')(tests.compareYield(anagramPalindrome('tacocat'), true))
+  write.description('anagramPalindrome should return true when passed a string that can be a re-aranged to a palindrome')(tests.compareYield(anagramPalindrome('tacocat'), true)),
+  write.description('anagramPalindrome should return fals when passed a string that CANNOT be a re-aranged to a palindrome')(tests.compareYield(anagramPalindrome('mooby'), false))
 );
